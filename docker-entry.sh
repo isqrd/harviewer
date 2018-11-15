64430efcd70af5ccb3f6a88227ca8324e2599a29
Joanna Anthony <joanna@wevr.com>
2018-11-15 12:23:11 -0800

    docker and ticketweb example

---
+++ docker-entry.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+trap handler SIGINT
+
+handler() {
+    	kill -s SIGINT $PID
+
+}
+
+cd /app
+npm install
+npm install nodemon -g
+#npm run setup && npm start
+#nodemon -e "js,mjs" --experimental-modules  addscript.mjs
+npm start
